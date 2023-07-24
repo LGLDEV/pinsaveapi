8a90746fe713efe404e008ab5f8511d29c23e178
LGLDEV <uanvar559@gmail.com>
2023-07-24 17:46:52 +0500

    A request function has been created to download

--- request.py
+++ request.py
@@ -1,6 +1,15 @@
 import aiohttp
+import asyncio
+from pydantic import TypeAdapter
+
+from models import Pin
+
+async def get_pin(url) -> Pin:
+	async with aiohttp.ClientSession() as session:
+		async with session.get(url=f"https://pinterestdownloader.io/frontendService/DownloaderService?url={url}") as response:
+			adapter = TypeAdapter(Pin)
+			return adapter.validate_python(await response.json())
 
 
-def get_pin() -> str:
-    return "Salom"
+
 
